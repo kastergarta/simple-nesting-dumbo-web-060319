19734e271d99d5a976d11464c32c09afaadb1d0a
Dennis <dennis.basyrov@gmail.com>
2019-05-17 05:31:03 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -81,11 +81,9 @@ def adding_matz
           :known_for => "Unix",
           :languages => ["C"]
         }
-     }
+     
 }
-    programmer_hash[:yukihiro_matsumoto] = {
-  :known_for => "Ruby",
-   :languages => ["LISP", "C"]
+programmer_hash[:yukihiro_matsumoto] = {:known_for => "Ruby", :languages => ["LISP", "C"]}
 end
 
 def changing_alan
